8250b0e4e73607150c27f0c1409c8ace38338217
ak363 <ak363@pc8-038-l.cs.st-andrews.ac.uk>
2022-03-05 14:23:19 +0000

    Added map

--- commands.py
+++ commands.py
@@ -1,7 +1,15 @@
+from cgi import test
 import discord
+from mapping import Map
 from discord.ext import commands
 
 def load(bot: commands.Bot):
     @bot.command()
     async def ping(ctx: commands.Context):
         await ctx.send("Pong.")
+
+    @bot.command()
+    async def maptest(ctx: commands.Context):
+        testmap = Map("https://media.discordapp.net/attachments/949603786429698048/949640245215920178/Board.png?width=735&height=610")
+        print(testmap)
+        await ctx.send(str(testmap))
--- rooms.py
+++ rooms.py
@@ -1,10 +1,14 @@
+from email.mime import image
 import cards
 
 class Room(cards.Card):
     """An instance of a room."""
     def __init__(self, name : str, imagepath : str):
-        super(name, imagepath)
+        super().__init__(name, imagepath)
         self.connections = {}
 
     def addConnection(self, relpos : str, room : "Room"):
         self.connections[relpos] = room
+
+    def __str__(self):
+        return "Room: " + self.name + "\nConnections: " + str(self.connections)
