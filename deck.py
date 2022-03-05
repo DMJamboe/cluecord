75eb1a4c0ebba552b44aafce0e82c474354205de
alg26 <alg26@pc8-040-l.cs.st-andrews.ac.uk>
2022-03-05 12:05:35 +0000

    added deck

---
+++ deck.py
@@ -0,0 +1,25 @@
+import characters
+import weapons
+import rooms
+import random
+
+class Deck(Object):
+    """The deck of cards"""
+    def __init__(self, characterList : "list[characters.Character]" , weaponList : "list[weapons.Weapon]", roomList : "list[rooms.Room]"):
+        self.cards = []
+        #add characters
+        for character in characterList:
+            self.cards.append(character)
+        #add weapons
+        for weapon in weaponList:
+            self.cards.append(weapon)
+        #add rooms
+        for room in roomList:
+            self.cards.append(room)
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def pop(self):
+        return self.cards.pop()
+
