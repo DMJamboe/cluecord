2eab482306bbbae92f09c221a550607cf87e4e99
DMJamboe <theamazingnox@gmail.com>
2022-03-05 12:18:46 +0000

    Wrote shell of game code.

--- game.py
+++ game.py
@@ -1,2 +1,15 @@
+from player import Player
+from deck import Deck
+
 class Game(object):
     """A game instance."""
+    def __init__(self, channel : TextChannel):
+        self.channel = channel
+        self.players : list[Player] = []
+        self.deck = Deck()
+
+    def addPlayer(self, player : Player):
+        self.players.append(player)
+
+    def start(self):
+        """Starts the game, shuffles cards to all players."""
