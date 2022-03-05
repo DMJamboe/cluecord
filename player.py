165cae3a1645169dafbd9c51765df05dffc737f7
DMJamboe <theamazingnox@gmail.com>
2022-03-05 11:44:58 +0000

    Created simple player instance.

--- player.py
+++ player.py
@@ -1,2 +1,8 @@
+import discord
+from cards import Card
+
 class Player(object):
     """A user playing the game."""
+    def __init__(self, user : discord.User):
+        self.user : discord.User = user
+        self.cards : list[Card] = []
